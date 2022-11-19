78b4de2c6697ef533f53b892f73db359a326689f
Christos Falas <raspberrymaker@gmail.com>
2022-11-19 22:02:58 +0000

    Change to torch

--- algorithmic_tasks/count_greater_than_x.py
+++ algorithmic_tasks/count_greater_than_x.py
@@ -1,11 +1,11 @@
-import numpy as np
+import torch
 
 
-def gen(seed, n_inputs, seq_len):
-    np.random.seed(seed)
-
-    x = np.random.randint(0, 100, size=(n_inputs,))
-    inp_list = np.random.randint(0, 100, size=(n_inputs, seq_len+1))
+def gen(n_inputs, vocab_size, seq_len):
+    x = torch.randint(1, vocab_size-1, size=(n_inputs,))
+    inp_list = torch.randint(0, vocab_size-1, size=(n_inputs, seq_len+1))
+    inp_list[:, 0] = 0
+    ans_list = torch.cumsum(inp_list >= x[:, None], axis=1)
     inp_list[:, 0] = x
-    ans_list = np.cumsum(inp_list >= x[:, None], axis=1)
+    ans_list[:, 0] = vocab_size-1
     return (inp_list, ans_list)
