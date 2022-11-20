fe3e4a793c5f241d0b95d791b3ebf656ffe06a6d
Christos Falas <raspberrymaker@gmail.com>
2022-11-20 02:40:14 +0000

    Added sort generator

---
+++ algorithmic_tasks/sort.py
@@ -0,0 +1,11 @@
+import torch
+
+
+def gen(n_inputs, vocab_size, seq_len):
+    inp_list = torch.randint(0, vocab_size-1, size=(n_inputs, 2*seq_len+1))
+    inp_list[:, seq_len+1:] = 0
+    inp_list[:, seq_len] = vocab_size-1
+    ans_list = torch.sort(inp_list, dim=1)[0]
+    inp_list[:, seq_len+1:] = ans_list[:, seq_len:-1]
+    ans_list[:, :seq_len] = vocab_size - 1
+    return (inp_list, ans_list)
